5d941ce7889c373c5f1eb3aeae173564cc7cd866
Salim Reza <salim15-3117@diu.edu.bd>
2022-06-23 23:05:53 +0600

    flower with turtle

---
+++ Python/flower.py
@@ -0,0 +1,14 @@
+from operator import le
+from turtle import *
+
+
+def flower():
+    for i in range(300):
+        circle(190-i, 90)
+        left(90)
+        circle(190-i, 90)
+        left(18)
+
+
+flower()
+mainloop()
