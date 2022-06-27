341b547dfea3c55f4bf4912ed0bf076e9849a7e6
Salim Reza <salim15-3117@diu.edu.bd>
2022-06-27 22:15:31 +0600

    Create LUdecomp.py
    LU decomposition using python

---
+++ Python/LUdecomp.py
@@ -0,0 +1,18 @@
+import pprint
+import scipy
+import scipy.linalg 
+
+A = scipy.array([ [7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6] ])
+P, L, U = scipy.linalg.lu(A)
+
+print "A:"
+pprint.pprint(A)
+
+print "P:"
+pprint.pprint(P)
+
+print "L:"
+pprint.pprint(L)
+
+print "U:"
+pprint.pprint(U)
